db6c504a994400eaef0b9ed347e81984482477a4
hamdy <hamdy.a.farag@gmail.com>
2019-03-18 16:13:02 +0200

    update build script

--- release-builder.sh
+++ release-builder.sh
@@ -13,8 +13,8 @@ npm install -g grunt-cli less
 # git checkout tags/v1.3.11   # In case you want to checkout a revision first
 #pushd /var/www/html/humhub
 wget -c https://getcomposer.org/download/1.8.4/composer.phar
-php composer.phar update
-php composer.phar install
+php composer.phar update --no-dev
+php composer.phar install --no-dev
 npm install
 grunt build-assets
 grunt build-theme
